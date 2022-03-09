1c44cd0d02df3b74313b01b7e8988cbe2b202a34
yemrehalis <yemrehalis@gmail.com>
2022-03-09 13:16:17 +0300

    api request done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data[0]);
+    }).catch((error) => {
+      console.log(error);
+    })
+  }, [])
+
+
+
   return (
     <>
 
